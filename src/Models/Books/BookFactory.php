336bc9c08adeae529ed3c0f1a8fffc9e1c6114b4
Ilya Rukavishnikov <ilya-ndt@mail.ru>
2025-11-12 16:56:37 +0300

    [fix]: Added book factory

---
+++ src/Models/Books/BookFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rukavishnikov\Php\Basic\App\Models\Books;
+
+use Rukavishnikov\Php\Basic\App\Types\Books\BookAuthor;
+use Rukavishnikov\Php\Basic\App\Types\Books\BookTitle;
+use Rukavishnikov\Php\Basic\App\Types\Books\BookYear;
+
+final class BookFactory
+{
+    /**
+     * @param array $data
+     * @return Book
+     */
+    public static function createFromArray(array $data): Book
+    {
+        return new Book(
+            new BookAuthor($data['author'] ?? ''),
+            new BookTitle($data['title'] ?? ''),
+            new BookYear((int)$data['year'] ?? 0)
+        );
+    }
+}
