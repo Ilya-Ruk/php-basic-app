2d49a0725104e86745328c119e0f74c0125fd8d9
Ilya Rukavishnikov <ilya-ndt@mail.ru>
2025-11-17 18:17:18 +0300

    [fix]: Added BookId type into entity Book

--- src/Entities/Books/Book.php
+++ src/Entities/Books/Book.php
@@ -2,24 +2,35 @@
 
 declare(strict_types=1);
 
-namespace Rukavishnikov\Php\Basic\App\Models\Books;
+namespace Rukavishnikov\Php\Basic\App\Entities\Books;
 
 use Rukavishnikov\Php\Basic\App\Types\Books\BookAuthor;
+use Rukavishnikov\Php\Basic\App\Types\Books\BookId;
 use Rukavishnikov\Php\Basic\App\Types\Books\BookTitle;
 use Rukavishnikov\Php\Basic\App\Types\Books\BookYear;
 
 final class Book
 {
     /**
+     * @param BookId|null $id
      * @param BookAuthor $author
      * @param BookTitle $title
      * @param BookYear $year
      */
     public function __construct(
+        private ?BookId $id,
         private BookAuthor $author,
         private BookTitle $title,
         private BookYear $year,
     ) {
+    }
+
+    /**
+     * @return BookId|null
+     */
+    public function getId(): ?BookId
+    {
+        return $this->id;
     }
 
     /**
@@ -47,11 +58,37 @@ final class Book
     }
 
     /**
+     * @param int $id
+     * @return Book
+     */
+    public function withId(int $id): Book
+    {
+        $new = clone $this;
+
+        $new->id = BookId::createFromInt($id);
+
+        return $new;
+    }
+
+    /**
+     * @return Book
+     */
+    public function withoutId(): Book
+    {
+        $new = clone $this;
+
+        $new->id = null;
+
+        return $new;
+    }
+
+    /**
      * @return array
      */
     public function getAsArray(): array
     {
         return [
+            'id' => $this->getId()?->getValue(),
             'author' => $this->getAuthor()->getValue(),
             'title' => $this->getTitle()->getValue(),
             'year' => $this->getYear()->getValue(),
