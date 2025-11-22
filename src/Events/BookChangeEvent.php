d201a59b18d96deeea9405d7e9e363ba9539c37f
Ilya Rukavishnikov <ilya-ndt@mail.ru>
2025-11-22 19:02:20 +0300

    [fix]: Added BookChangeEvent class

---
+++ src/Events/BookChangeEvent.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rukavishnikov\Php\Basic\App\Events;
+
+use Rukavishnikov\Php\Basic\App\Entities\Books\Book;
+
+final class BookChangeEvent
+{
+    /**
+     * @param Book $oldBook
+     * @param Book $newBook
+     */
+    public function __construct(
+        public Book $oldBook,
+        public Book $newBook,
+    ) {
+    }
+}
